7ac47a5efea5e22c7bd25f922dafa3c7f44897c9
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2026-02-28 15:42:47 -0800

    update template to match python randomizer output

--- gamelauncher/include/batch_game_list.h
+++ gamelauncher/include/batch_game_list.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <Arduino.h>
 
-static const char *game_list[] PROGMEM = {
+static const char* const P_game_list[] PROGMEM = {
     // null terminate this buf
     "\0"
 };
+
+static const uint32_t PROGMEM P_appID_list[] = {};
