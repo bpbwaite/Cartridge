43c10c0ca8e7babd463b9b209f615ac21b4b27e7
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2026-02-28 18:03:12 -0800

    avoid divide by zero by define-guarding

--- gamelauncher/include/batch_game_list.h
+++ gamelauncher/include/batch_game_list.h
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef BATCH_GAME_LIST_H
+#define BATCH_GAME_LIST_H
+
 #include <Arduino.h>
+
+#define FLASH_LISTS_NOT_INITIALIZED // this line is removed by running the python script.
 
 static const char* const P_game_list[] PROGMEM = {
     // null terminate this buf
@@ -7,3 +11,5 @@ static const char* const P_game_list[] PROGMEM = {
 };
 
 static const uint32_t PROGMEM P_appID_list[] = {};
+
+#endif
