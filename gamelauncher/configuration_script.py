700b0479faedcd72af1e5f87f9355d958abca59d
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2026-03-01 21:01:05 -0800

    improve robustness

--- gamelauncher/configuration_script.py
+++ gamelauncher/configuration_script.py
@@ -1,25 +1,34 @@
-Import("env")
+Import("env") # type: ignore
 
 import os
 import shutil
 
-
+precompile_script_success: bool = False
 def pre():
     # runs pre compilation but post project configuration
-    print('Custom p-e compilation running')
-    if os.path.exists('include/batch_game_list_local.h'):
-        shutil.move('include/batch_game_list.h', 'include/temp_batch_game_list.h')
-        shutil.move('include/batch_game_list_local.h', 'include/batch_game_list.h')
+    print('Custom pre compilation script running now')
+    if os.path.exists('./include/temp_batch_game_list.h'):
+        print('Warning: header file injection is broken, games list will not be copied')
+        return
+    if os.path.exists('./include/batch_game_list_local.h'):
+        shutil.move('./include/batch_game_list.h', './include/temp_batch_game_list.h')
+        shutil.copy2('./include/batch_game_list_local.h', './include/batch_game_list.h')
+    global precompile_script_success
+    precompile_script_success = True
 
 def post(source, target, env):
-    print('Custom post-copmilation running')
-    if os.path.exists('include/temp_batch_game_list.h'):
-        shutil.move('include/batch_game_list.h', 'include/batch_game_list_local.h')
-        shutil.move('include/temp_batch_game_list.h', 'include/batch_game_list.h')
+    # runs after the .hex firmware has been generated
+    if not precompile_script_success:
+        print('Skipping post-compilation script')
+        return
+    print('Custom post-compilation script running now')
+    if os.path.exists('./include/temp_batch_game_list.h'):
+        shutil.move('./include/temp_batch_game_list.h', './include/batch_game_list.h')
 
 pre()
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", post) # type: ignore
 
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", post)
+
 
 
     
