f4068a122305b0a7cb6d586a6a828760709ceb28
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2026-03-25 21:32:45 -0700

    remove references to header file, as we are using serial now

--- cartridge-firmware/configuration_script.py
+++ cartridge-firmware/configuration_script.py
@@ -11,12 +11,6 @@ precompile_script_success: bool = False
 def pre(*_):
     # runs pre compilation but post project configuration
     print('Custom pre compilation script running now')
-    if os.path.exists('./include/temp_batch_game_list.h'):
-        print('Warning: header file injection is broken, games list will not be copied')
-        return
-    if os.path.exists('./include/batch_game_list_local.h'):
-        shutil.move('./include/batch_game_list.h', './include/temp_batch_game_list.h')
-        shutil.copy2('./include/batch_game_list_local.h', './include/batch_game_list.h')
     global precompile_script_success
     precompile_script_success = True
 
@@ -26,8 +20,6 @@ def post(source, target, env):
         print('Skipping post-compilation script')
         return
     print('Custom post-compilation script running now')
-    if os.path.exists('./include/temp_batch_game_list.h'):
-        shutil.move('./include/temp_batch_game_list.h', './include/batch_game_list.h')
 
 pre()
 
